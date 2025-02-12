d099a0a7caa420145d0181bc4d3279262e674b94
LUCAS RANYE RIBEIRO <lucas.rribeiro6@senacsp.edu.br>
2025-02-12 16:48:58 -0300

    gta

--- bob.js
+++ bob.js
@@ -3,12 +3,11 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import User from "./models/usuariomodels.js"
+import User from "./models/usuariomodels.js";
+import { ReturnDocument } from "mongodb";
 dotenv.config();
 
 const app = express();
-
-
 
 app.use(express.json());
 
@@ -19,47 +18,86 @@ app.get("/", (req, res) => {
 app.post("/auth/register", async (req, res) => {
   const { name, email, password, confirmpassword } = req.body;
 
+  if (!name) {
+    return res.status(422).json({ msg: "o nome é obrigatorio!" });
+  }
 
-if (!name) {
-return res.status(422).json({ msg: "o nome é obrigatorio!" })
-}
+  if (!email) {
+    return res.status(422).json({ msg: "o email é obrigatorio!" });
+  }
 
-if (!email) {
-return res.status(422).json({ msg: "o email é obrigatorio!"})
-}
+  if (!password) {
+    return res.status(422).json({ msg: "o senha é obrigatorio!" });
+  }
 
-if (!password) {
-return res.status(422).json({ msg: "o senha é obrigatorio!"})
-}
+  if (password != confirmpassword) {
+    return res
+      .status(422)
+      .json({ msg: "A senha e a confimacão precisa ser iguais" });
+  }
 
-if (password != confirmpassword) {
-  return res
-    .status(422)
-    .json({ msg: "A senha e a confimacão precisa ser iguais"});
-}
+  const userExists = await User.findOne({ email: email });
 
-const userExists = await User.findOne({ email: email });
+  if (userExists) {
+    return res.status(422).json({ msg: "por favor , utilize outro e-mail" });
+  }
 
-if(userExists) {
-  return res.status(422).json({ msg: "por afvor , utilize outro e-mail"});
-}
+  const salt = await bcrypt.genSalt(12);
+  const passwordhast = await bcrypt.hash(password, salt);
 
-const salt = await bcrypt.genSalt(12);
-const passwordhast = await bcrypt.hash(password, salt);
+  const user = new User({
+    name,
+    email,
+    passwordhast,
+  });
 
-const user = new User({
-  name,
-  email,
-  passwordhast,
+  try {
+    await user.save();
+
+    res.status(201).json({ msg: "usuario criado com sucesso" });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 });
 
-try {
-  await user.save
+app.post("/auth/login", async (req, res) => {
+  const { email, password} = req.body;
 
-  res.status(201).json({msg: "usuario criado com sucesso"});
+  if (!email) {
+    return res.status(422).json({msg: "O email deve ser informado!"});
+  }
+
+  if (!password) {
+    return res.status(422).json({msg: "O senha deve ser informado!"});
+  }
+
+const user = await User.findOne({ email: email });
+
+if (!user) {
+  return res.status(404).json({msg: "usuario nao encontrado!"});
+}
+
+const checkPassword = await bcrypt.compare(password, User.password);
+
+  if (!checkPassword) {
+    return res.status(422).json({msg: "senha invalida, tente novamente."});
+  }
+
+try {
+  const secret = process.env.SECRET;
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    secret
+  );
+
+  res.status(200).json({msg: "autenticacao realizada com sucesso!", token});
 } catch (error) {
   res.status(500).json({msg: error});
 }
+
 });
 
 const dbUser = process.env.DB_USER;
